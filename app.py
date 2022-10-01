ba6ebafa8228cfe8bd2086da1752de3cd279b3cf
philiphowes <48262208+philiphowes@users.noreply.github.com>
2022-10-01 13:38:30 +0700

    Open_ai_gpt_fork

--- app.py
+++ app.py
@@ -1,35 +1,23 @@
 import os
-
 import openai
-from flask import Flask, redirect, render_template, request, url_for
 
-app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY="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"
 
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+print(openai.api_key)
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+response = openai.Completion.create(
+  engine="text-davinci-002",
+  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0.6,
+  stop=[" Human:", " AI:"]
+)
